da2f1d5bb35b0265f353ffd620865ee75c94390c
Leandro <lesafe07@gmail.com>
2025-11-29 11:40:51 -0300

    fix: export site for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/biotec-portas-abertas',
+  assetPrefix: '/biotec-portas-abertas/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
