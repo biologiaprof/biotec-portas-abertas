097d5800451bd7b8b45af92015042ce461ccc80f
Leandro <lesafe07@gmail.com>
2025-11-29 11:54:27 -0300

    fix: disable turbopack for correct static export

--- _next/static/2yfXN3bBGAM7d2bd90gqm/_buildManifest.js
+++ _next/static/2yfXN3bBGAM7d2bd90gqm/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/biotec-portas-abertas//_next/:path+",
-        "destination": "/biotec-portas-abertas/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
